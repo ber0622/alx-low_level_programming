b46479e3e0841c3fa20e937bf71c3a81a9e2d3e8
ber0622 <bernmuturi@gmail.com>
2022-06-30 20:26:24 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
 *main - entry
-*Return : Always 0 (success)
+*Return : 0 if exited properly, non zero otherwise
 */
-int main(void) 
+int main(void);
 {
-puts("\"Programming is like building a multilingual puzzle);
+puts("\"Programming is like building a multilingual puzzle")
 return (0);
 }
