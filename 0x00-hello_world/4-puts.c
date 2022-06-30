7b578a68d36bea707dad0e12b2bb27b733c1b11e
ber0622 <bernmuturi@gmail.com>
2022-06-30 20:45:06 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-*main - entry
-*Return : 0 if exited properly, non zero otherwise
-*/
-int main(void);
+ * main - entry
+ * Return: 0 if exited correctly, non zero if otherwise
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
-return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
