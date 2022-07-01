ba91adac9acf59d1b27ef33108e28c6e9e60fbf0
ber0622 <bernmuturi@gmail.com>
2022-07-01 22:30:41 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 if successful
+ */
+int main(void)
+{
+char lower_case;
+char upper_case;
+
+for (lower_case = 'a'; lower_case < = 'z'; lower_case++)
+{
+putchar(lower_case);
+}
+for (upper_case = 'A'; upper_case < = 'Z'; uppercase++)
+{
+putchar(upper_case);
+}
+
+putchar('\n');
+
+return (0);
+
+}
