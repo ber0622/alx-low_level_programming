36d2e2113aa1e6e0b2619b404ed4cf1cb4e21a61
ber0622 <bernmuturi@gmail.com>
2022-07-01 23:30:00 +0000

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,7 +13,7 @@ for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar(lower_case);
 }
-for (upper_case = 'A'; upper_case <= 'Z'; uppercase++)
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 {
 putchar(upper_case);
 }
