30076d513d507ba7cee79309619d29ab77ec845d
ber0622 <bernmuturi@gmail.com>
2022-07-02 00:30:29 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * return: Always 0 if successful
+ */
+int main(void)
+
+{
+char base_16;
+
+for (base_16 = 48; base_16 <= 57; base_16++)
+{
+putchar(base_16);
+}
+
+putchar('\n');
+
+return (0);
+
+}
