6d1b96304eb7e6057be7d3a5ad350d2bd3ed6de7
ber0622 <bernmuturi@gmail.com>
2022-07-02 10:19:29 +0000

    BE BACK

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,12 @@
  */
 int main(void)
 {
+	char c;
+	int i;
+       	long int;
+	long long int;
+
+
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of a int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
