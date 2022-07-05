7e0a02ae440193eb9ce937360dc21f1355602f3f
ber0622 <bernmuturi@gmail.com>
2022-07-05 22:12:05 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet - print_alphabet_x10
+*/
+
+void print_alphabet(void)
+
+{
+	int i,
+
+	for (i = 'a'; i <= 'z'; i++)
+{
+	_putchar(i);
+}
+
+{
+	_putchar('\n');
+}
+}
