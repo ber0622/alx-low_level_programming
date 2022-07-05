2976d8937daca340532f4181aea6b5f4d39593ff
ber0622 <bernmuturi@gmail.com>
2022-07-05 23:08:43 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,6 @@
 int print_sign(int n)
 {
 	if (n > 0)
-
 {
 	_putchar('+');
 	return (1);
@@ -17,7 +16,7 @@ int print_sign(int n)
 	_putchar('0');
 	return (0);
 }
-	else if (n < 0)
+	else
 {
 	_putchar('-');
 	return (-1);
