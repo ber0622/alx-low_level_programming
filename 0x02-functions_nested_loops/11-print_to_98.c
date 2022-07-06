880342e7327474fbaf49425172af71a74a235d19
ber0622 <bernmuturi@gmail.com>
2022-07-06 11:43:23 +0000

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,46 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
-*
-*
-*
-*
+ * print_to_98 - fn
+ * @n: parameter
+ * Return: 0 success
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
+			n--;
+		}
+	}
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
+			n++;
+		}
+	}
+	else
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+}
