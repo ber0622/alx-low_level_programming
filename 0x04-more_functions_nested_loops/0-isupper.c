f0cefde2f35c104cfdaf24e592a61f9dda1e4586
ber0622 <bernmuturi@gmail.com>
2022-07-07 13:05:01 +0000

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* main - check the code.
+* _isupper - check the code.
 * @c: displays character
 * Return: Always 0.
 */
@@ -11,15 +10,14 @@ int _isupper(int c);
 	char c;
 
 	if (c >= 'A' && c <= 'Z')
-
-	{
+{
 		return (1);
-	}
+}
 
 	else
-	{
+{
 		return (0);
-	}
+}
 
 	_putchar('\n');
 }
