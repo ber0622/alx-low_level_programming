d47d44788ecb856d9a9d3075d396b3752cb9e5c3
ber0622 <bernmuturi@gmail.com>
2022-07-07 13:02:19 +0000

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - check the code.
+* @c: displays character
+* Return: Always 0.
+*/
+int _isupper(int c);
+{
+	char c;
+
+	if (c >= 'A' && c <= 'Z')
+
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#/bin/bash
+0x04. C - More functions, more nested loops
