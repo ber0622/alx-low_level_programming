a9c35db2072ecb05328eca4366cc1825f5cf08a8
ber0622 <bernmuturi@gmail.com>
2022-07-11 07:49:46 +0000

    I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,12 +2,10 @@
 /**
 * _puts - prints a string
 * @str: parameter
+* Return: string
 */
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
+	return (*str);
 	_putchar('\n');
 }
