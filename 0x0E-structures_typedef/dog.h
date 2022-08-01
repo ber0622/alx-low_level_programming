36679d1980df01adb5468452791dcaf09cdf6b43
ber0622 <bernmuturi@gmail.com>
2022-08-01 14:33:54 +0000

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,9 +12,4 @@ struct dog
 	float age;
 	char *owner;
 } dog;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-void free_dog(dog_t *d);
-dog_t *new_dog(char *name, float age, char *owner);
 #endif
