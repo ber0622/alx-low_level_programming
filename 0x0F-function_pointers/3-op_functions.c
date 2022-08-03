0e4c80789b5bf35058d9983cf78081b2166c1fa8
ber0622 <bernmuturi@gmail.com>
2022-08-03 14:30:57 +0000

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,66 +1,67 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-* op_add - Fctn for addition
-* @a:First interger
-* @b: Second interger
-* Return: Checks for success
-*/
+ * op_add - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: sum of 2 numbers
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
-* op_sub - Fctn for subtraction
-* @a: First interger
-* @b: second interger
-* Return: Checks for success
-*/
+ * op_sub - subtraction operator
+ * @a: first in
+ * @b: second int
+ * Return: difference of 2 numbers
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
-* op_mul - Fctn for Multiplication
-* @a: first interger
-* @b: second interger
-* Returns: Checks for success
-*/
+ * op_mul - product of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: product of the numbers
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
-* op_div - fctn for division
-* @a: first interger
-* @b:second interger
-* return: checks for success
-*/
+ * op_div - division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: result of the division of the numbers
+ */
 int op_div(int a, int b)
 {
 	if (b == 0)
-	printf ("Error\n");
-	exit(100);
-	else
 	{
-	return (a / b);
+		printf("Error\n");
+		exit(100);
 	}
+	return (a / b);
 }
+
 /**
-* op_mod - fctn for modulus
-* @a: first interger
-* @b: second interger
-* return: check for sucess
-*/
-int op_mod(int a, int b)	
+ * op_mod - get remainder of the division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: remainder of division of the numbers
+ */
+int op_mod(int a, int b)
 {
 	if (b == 0)
-	printf ("Error\n");
-	exit(100);
-	else
 	{
-	return (a % b);
+		printf("Error\n");
+		exit(100);
 	}
+	return (a % b);
 }
