54e35929235ae2a13ce0cd4ec1e0248d2d820a77
ber0622 <bernmuturi@gmail.com>
2022-08-17 10:22:35 +0000

    renewed

--- 0x13-more_singly_linked_lists/test.c
+++ 0x13-more_singly_linked_lists/test.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * listint_len - linked lists
+ * @h: lists
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
 
 /**
  * insert_nodeint_at_index - Insert a new node at a given positiion.
@@ -12,25 +31,25 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *temp;
-	unsigned int i = 1;
+	unsigned int count, i = 1;
 
-	if (*head == NULL && idx != 0)
+	if (*head == NULL)
 		return (NULL);
-
 	temp = *head;
-	for (; i < idx && temp != NULL; i++)
-		temp = temp->next;
-	
+
+	if (idx != 0)
+	{
+		count = (int* ) listint_len;
+		while (i < count)
+		{
+			temp = temp->next;
+			i++;
+		}
+	}
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if (idx == 0)
-	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
 	new_node->next = temp->next;
 	temp->next = new_node;
 	return (new_node);
