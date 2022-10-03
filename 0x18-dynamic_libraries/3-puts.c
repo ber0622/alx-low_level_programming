fdd9a0d38749efe3d8590af08711e6e877e241d4
ber0622 <bernmuturi@gmail.com>
2022-10-03 12:26:15 +0300

    A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - print to the stdout
+ * @str: string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int len, i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copy string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: Character
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = strlen(src);
+	for (i = 0; i <= len; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
